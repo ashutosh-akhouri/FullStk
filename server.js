25fe62c28a786600014c2a6c36615fecbe9176d3
ashutosh_k_akhouri <ashutosh.akhouri@gmail.com>
2020-01-25 11:58:36 +0530

    fullstk

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname + '/dist/fullstk'));
+
+//Backend Route
+app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/dist/fullstk/index.html')));
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
